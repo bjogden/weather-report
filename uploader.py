e5ec07c0fadf684f073f691844d46efd41c93f49
root <root@raspberrypi.(none)>
2015-11-09 15:05:22 -0800

    updated uploader to upload CSV log as well as JSON

--- uploader.py
+++ uploader.py
@@ -6,6 +6,13 @@ from pyconversion import Convert
 
 cwd = os.getcwd()
 
+def upload_s3(fname, path):
+    c = boto.connect_s3(aws_access_key_id, aws_secret_key)
+    b = c.get_bucket(aws_bucket_name)
+    k = b.new_key(fname)
+    k.set_contents_from_filename(path)
+    k.set_acl("public-read")
+
 try:
     # converts large CSV to JSON
     json_file = Convert.csv_to_json(log_path+'weather.log', log_path+'weather.json')
@@ -14,11 +21,13 @@ try:
     shutil.move(json_file, new_path)
     print "moved file locally"
     # uploads it to S3
-    c = boto.connect_s3(aws_access_key_id, aws_secret_key)
-    b = c.get_bucket(aws_bucket_name)
-    k = b.new_key("weather.json")
-    k.set_contents_from_filename(new_path)
-    k.set_acl("public-read")
+    upload_s3("weather.json", new_path)
+    upload_s3("weather.log", "log/weather.log")
+#    c = boto.connect_s3(aws_access_key_id, aws_secret_key)
+#    b = c.get_bucket(aws_bucket_name)
+#    k = b.new_key("weather.json")
+#    k.set_contents_from_filename(new_path)
+#    k.set_acl("public-read")
 
     print "Uploaded file to S3"
 except Exception, e:
